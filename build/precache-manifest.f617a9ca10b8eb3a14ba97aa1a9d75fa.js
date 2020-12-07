410a478696396379ecede964210a0225e2d37f5f
Brennan Predmore <brennanpredmore@brennans-air.lan>
2020-12-06 22:03:02 -0600

    First init to github from local

---
+++ build/precache-manifest.f617a9ca10b8eb3a14ba97aa1a9d75fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f169882863b6be560314aef8f2bd78a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cac5fd5cc4e4b2df939",
+    "url": "/static/css/main.bdd84d9b.chunk.css"
+  },
+  {
+    "revision": "0f45275edd9276aa116e",
+    "url": "/static/js/2.bd20a805.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bd20a805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cac5fd5cc4e4b2df939",
+    "url": "/static/js/main.29e4ae73.chunk.js"
+  },
+  {
+    "revision": "b70b83d333f725b7b4d4",
+    "url": "/static/js/runtime-main.ea28d270.js"
+  }
+]);
